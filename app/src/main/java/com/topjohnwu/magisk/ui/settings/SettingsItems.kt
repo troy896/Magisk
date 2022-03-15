RobThePCGuy/Kyubi

Overhaul SettingsItem
Close #5021

app/src/main/java/com/topjohnwu/magisk/ui/settings/SettingsItems.kt
@@ -37,8 +37,9 @@ object Customization : BaseSettingsItem.Section() {
 
 object Language : BaseSettingsItem.Selector() {
     override var value = -1
-        set(value) = setV(value, field, { field = it }) {
-            Config.locale = entryValues[it]
+        set(value) {
+            field = value
+            Config.locale = entryValues[value]
         }
 
     override val title = R.string.language.asText()
@@ -49,9 +50,9 @@ object Language : BaseSettingsItem.Selector() {
     override fun entries(res: Resources) = entries
     override fun descriptions(res: Resources) = entries
 
-    override fun onPressed(view: View, callback: Callback) {
-        if (entries.isEmpty()) return
-        super.onPressed(view, callback)
+    override fun onPressed(view: View, handler: Handler) {
+        if (entries.isNotEmpty())
+            super.onPressed(view, handler)
     }
 
     suspend fun loadLanguages(scope: CoroutineScope) {
@@ -80,9 +81,7 @@ object AppSettings : BaseSettingsItem.Section() {
 object Hide : BaseSettingsItem.Input() {
     override val title = R.string.settings_hide_app_title.asText()
     override val description = R.string.settings_hide_app_summary.asText()
-
     override var value = ""
-        set(value) = setV(value, field, { field = it })
 
     override val inputResult
         get() = if (isError) null else result
@@ -113,30 +112,31 @@ object AddShortcut : BaseSettingsItem.Blank() {
 }
 
 object DownloadPath : BaseSettingsItem.Input() {
-    override var value = Config.downloadDir
-        set(value) = setV(value, field, { field = it }) { Config.downloadDir = it }
+    override var value
+        get() = Config.downloadDir
+        set(value) {
+            Config.downloadDir = value
+            notifyPropertyChanged(BR.description)
+        }
 
     override val title = R.string.settings_download_path_title.asText()
-    override val description get() = path.asText()
+    override val description get() = MediaStoreUtils.fullPath(value).asText()
 
-    override val inputResult: String get() = result
-
-    @get:Bindable
-    var result = value
-        set(value) = set(value, field, { field = it }, BR.result, BR.path)
+    override var inputResult: String = value
+        set(value) = set(value, field, { field = it }, BR.inputResult, BR.path)
 
     @get:Bindable
-    val path
-        get() = MediaStoreUtils.fullPath(result)
+    val path get() = MediaStoreUtils.fullPath(inputResult)
 
     override fun getView(context: Context) = DialogSettingsDownloadPathBinding
         .inflate(LayoutInflater.from(context)).also { it.data = this }.root
 }
 
 object UpdateChannel : BaseSettingsItem.Selector() {
-    override var value = Config.updateChannel.let { if (it < 0) 0 else it }
-        set(value) = setV(value, field, { field = it }) {
-            Config.updateChannel = it
+    override var value
+        get() = Config.updateChannel
+        set(value) {
+            Config.updateChannel = value
             Info.remote = Info.EMPTY_REMOTE
         }
 
@@ -154,18 +154,17 @@ object UpdateChannel : BaseSettingsItem.Selector() {
 
 object UpdateChannelUrl : BaseSettingsItem.Input() {
     override val title = R.string.settings_update_custom.asText()
-    override var value = Config.customChannelUrl
-        set(value) = setV(value, field, { field = it }) {
-            Config.customChannelUrl = it
-            Info.remote = Info.EMPTY_REMOTE
-        }
     override val description get() = value.asText()
+    override var value
+        get() = Config.customChannelUrl
+        set(value) {
+            Config.customChannelUrl = value
+            Info.remote = Info.EMPTY_REMOTE
+            notifyPropertyChanged(BR.description)
+        }
 
-    override val inputResult get() = result
-
-    @get:Bindable
-    var result = value
-        set(value) = set(value, field, { field = it }, BR.result)
+    override var inputResult: String = value
+        set(value) = set(value, field, { field = it }, BR.inputResult)
 
     override fun refresh() {
         isEnabled = UpdateChannel.value == Config.Value.CUSTOM_CHANNEL
@@ -178,9 +177,10 @@ object UpdateChannelUrl : BaseSettingsItem.Input() {
 object UpdateChecker : BaseSettingsItem.Toggle() {
     override val title = R.string.settings_check_update_title.asText()
     override val description = R.string.settings_check_update_summary.asText()
-    override var value = Config.checkUpdate
-        set(value) = setV(value, field, { field = it }) {
-            Config.checkUpdate = it
+    override var value
+        get() = Config.checkUpdate
+        set(value) {
+            Config.checkUpdate = value
             JobService.schedule(AppContext)
         }
 }
@@ -188,49 +188,12 @@ object UpdateChecker : BaseSettingsItem.Toggle() {
 object DoHToggle : BaseSettingsItem.Toggle() {
     override val title = R.string.settings_doh_title.asText()
     override val description = R.string.settings_doh_description.asText()
-    override var value = Config.doh
-        set(value) = setV(value, field, { field = it }) {
-            Config.doh = it
-        }
+    override var value by Config::doh
 }
 
-// check whether is module already installed beforehand?
 object SystemlessHosts : BaseSettingsItem.Blank() {
     override val title = R.string.settings_hosts_title.asText()
     override val description = R.string.settings_hosts_summary.asText()
-}
-
-object Tapjack : BaseSettingsItem.Toggle() {
-    override val title = R.string.settings_su_tapjack_title.asText()
-    override var description = R.string.settings_su_tapjack_summary.asText()
-    override var value = Config.suTapjack
-        set(value) = setV(value, field, { field = it }) { Config.suTapjack = it }
-}
-
-object Biometrics : BaseSettingsItem.Toggle() {
-    override val title = R.string.settings_su_biometric_title.asText()
-    override var value = Config.suBiometric
-        set(value) = setV(value, field, { field = it }) { Config.suBiometric = it }
-    override var description = R.string.settings_su_biometric_summary.asText()
-
-    override fun refresh() {
-        isEnabled = BiometricHelper.isSupported
-        if (!isEnabled) {
-            value = false
-            description = R.string.no_biometric.asText()
-        }
-    }
-}
-
-object Reauthenticate : BaseSettingsItem.Toggle() {
-    override val title = R.string.settings_su_reauth_title.asText()
-    override val description = R.string.settings_su_reauth_summary.asText()
-    override var value = Config.suReAuth
-        set(value) = setV(value, field, { field = it }) { Config.suReAuth = it }
-
-    override fun refresh() {
-        isEnabled = Build.VERSION.SDK_INT < Build.VERSION_CODES.O && Utils.showSuperUser()
-    }
 }
 
 // --- Magisk
@@ -244,11 +207,13 @@ object Zygisk : BaseSettingsItem.Toggle() {
     override val description get() =
         if (mismatch) R.string.reboot_apply_change.asText()
         else R.string.settings_zygisk_summary.asText()
-    override var value = Config.zygisk
-        set(value) = setV(value, field, { field = it }) {
-            Config.zygisk = it
-            DenyList.isEnabled = it
-            DenyListConfig.isEnabled = it
+    override var value
+        get() = Config.zygisk
+        set(value) {
+            Config.zygisk = value
+            DenyList.isEnabled = value
+            DenyListConfig.isEnabled = value
+            notifyPropertyChanged(BR.description)
             DenyList.notifyPropertyChanged(BR.description)
         }
     val mismatch get() = value != Info.isZygiskEnabled
@@ -267,11 +232,16 @@ object DenyList : BaseSettingsItem.Toggle() {
         }
 
     override var value = Config.denyList
-        set(value) = setV(value, field, { field = it }) {
-            val cmd = if (it) "enable" else "disable"
+        set(value) {
+            field = value
+            val cmd = if (value) "enable" else "disable"
             Shell.su("magisk --denylist $cmd").submit { result ->
-                if (result.isSuccess) Config.denyList = it
-                else field = !it
+                if (result.isSuccess) {
+                    Config.denyList = value
+                } else {
+                    field = !value
+                    notifyPropertyChanged(BR.checked)
+                }
             }
         }
 
@@ -290,6 +260,27 @@ object DenyListConfig : BaseSettingsItem.Blank() {
 
 // --- Superuser
 
+object Tapjack : BaseSettingsItem.Toggle() {
+    override val title = R.string.settings_su_tapjack_title.asText()
+    override val description = R.string.settings_su_tapjack_summary.asText()
+    override var value by Config::suTapjack
+}
+
+object Biometrics : BaseSettingsItem.Toggle() {
+    override val title = R.string.settings_su_biometric_title.asText()
+    override var description = R.string.settings_su_biometric_summary.asText()
+    override var value by Config::suBiometric
+
+    override fun refresh() {
+        isEnabled = BiometricHelper.isSupported
+        if (!isEnabled) {
+            value = false
+            description = R.string.no_biometric.asText()
+            notifyPropertyChanged(BR.checked)
+        }
+    }
+}
+
 object Superuser : BaseSettingsItem.Section() {
     override val title = R.string.superuser.asText()
 }
@@ -297,22 +288,14 @@ object Superuser : BaseSettingsItem.Section() {
 object AccessMode : BaseSettingsItem.Selector() {
     override val title = R.string.superuser_access.asText()
     override val entryRes = R.array.su_access
-
-    override var value = Config.rootMode
-        set(value) = setV(value, field, { field = it }) {
-            Config.rootMode = it
-        }
+    override var value by Config::rootMode
 }
 
 object MultiuserMode : BaseSettingsItem.Selector() {
     override val title = R.string.multiuser_mode.asText()
     override val entryRes = R.array.multiuser_mode
     override val descriptionRes = R.array.multiuser_summary
-
-    override var value = Config.suMultiuserMode
-        set(value) = setV(value, field, { field = it }) {
-            Config.suMultiuserMode = it
-        }
+    override var value by Config::suMultiuserMode
 
     override fun refresh() {
         isEnabled = Const.USER_ID == 0
@@ -323,21 +306,13 @@ object MountNamespaceMode : BaseSettingsItem.Selector() {
     override val title = R.string.mount_namespace_mode.asText()
     override val entryRes = R.array.namespace
     override val descriptionRes = R.array.namespace_summary
-
-    override var value = Config.suMntNamespaceMode
-        set(value) = setV(value, field, { field = it }) {
-            Config.suMntNamespaceMode = it
-        }
+    override var value by Config::suMntNamespaceMode
 }
 
 object AutomaticResponse : BaseSettingsItem.Selector() {
     override val title = R.string.auto_response.asText()
     override val entryRes = R.array.auto_response
-
-    override var value = Config.suAutoResponse
-        set(value) = setV(value, field, { field = it }) {
-            Config.suAutoResponse = it
-        }
+    override var value by Config::suAutoResponse
 }
 
 object RequestTimeout : BaseSettingsItem.Selector() {
@@ -345,21 +320,25 @@ object RequestTimeout : BaseSettingsItem.Selector() {
     override val entryRes = R.array.request_timeout
 
     private val entryValues = listOf(10, 15, 20, 30, 45, 60)
-    override var value = selected
-        set(value) = setV(value, field, { field = it }) {
-            Config.suDefaultTimeout = entryValues[it]
+    override var value = entryValues.indexOfFirst { it == Config.suDefaultTimeout }
+        set(value) {
+            field = value
+            Config.suDefaultTimeout = entryValues[value]
         }
-
-    private val selected: Int
-        get() = entryValues.indexOfFirst { it == Config.suDefaultTimeout }
 }
 
 object SUNotification : BaseSettingsItem.Selector() {
     override val title = R.string.superuser_notification.asText()
     override val entryRes = R.array.su_notification
+    override var value by Config::suNotification
+}
 
-    override var value = Config.suNotification
-        set(value) = setV(value, field, { field = it }) {
-            Config.suNotification = it
-        }
+object Reauthenticate : BaseSettingsItem.Toggle() {
+    override val title = R.string.settings_su_reauth_title.asText()
+    override val description = R.string.settings_su_reauth_summary.asText()
+    override var value by Config::suReAuth
+
+    override fun refresh() {
+        isEnabled = Build.VERSION.SDK_INT < Build.VERSION_CODES.O && Utils.showSuperUser()
+    }
 }

troy896/Magisk

app/src/main/java/com/topjohnwu/magisk/ui/settings/SettingsItems.kt
@@ -37,8 +37,9 @@ object Customization : BaseSettingsItem.Section() {
 
 object Language : BaseSettingsItem.Selector() {
     override var value = -1
-        set(value) = setV(value, field, { field = it }) {
-            Config.locale = entryValues[it]
+        set(value) {
+            field = value
+            Config.locale = entryValues[value]
         }
 
     override val title = R.string.language.asText()
@@ -49,9 +50,9 @@ object Language : BaseSettingsItem.Selector() {
     override fun entries(res: Resources) = entries
     override fun descriptions(res: Resources) = entries
 
-    override fun onPressed(view: View, callback: Callback) {
-        if (entries.isEmpty()) return
-        super.onPressed(view, callback)
+    override fun onPressed(view: View, handler: Handler) {
+        if (entries.isNotEmpty())
+            super.onPressed(view, handler)
     }
 
     suspend fun loadLanguages(scope: CoroutineScope) {
@@ -80,9 +81,7 @@ object AppSettings : BaseSettingsItem.Section() {
 object Hide : BaseSettingsItem.Input() {
     override val title = R.string.settings_hide_app_title.asText()
     override val description = R.string.settings_hide_app_summary.asText()
-
     override var value = ""
-        set(value) = setV(value, field, { field = it })
 
     override val inputResult
         get() = if (isError) null else result
@@ -113,30 +112,31 @@ object AddShortcut : BaseSettingsItem.Blank() {
 }
 
 object DownloadPath : BaseSettingsItem.Input() {
-    override var value = Config.downloadDir
-        set(value) = setV(value, field, { field = it }) { Config.downloadDir = it }
+    override var value
+        get() = Config.downloadDir
+        set(value) {
+            Config.downloadDir = value
+            notifyPropertyChanged(BR.description)
+        }
 
     override val title = R.string.settings_download_path_title.asText()
-    override val description get() = path.asText()
+    override val description get() = MediaStoreUtils.fullPath(value).asText()
 
-    override val inputResult: String get() = result
-
-    @get:Bindable
-    var result = value
-        set(value) = set(value, field, { field = it }, BR.result, BR.path)
+    override var inputResult: String = value
+        set(value) = set(value, field, { field = it }, BR.inputResult, BR.path)
 
     @get:Bindable
-    val path
-        get() = MediaStoreUtils.fullPath(result)
+    val path get() = MediaStoreUtils.fullPath(inputResult)
 
     override fun getView(context: Context) = DialogSettingsDownloadPathBinding
         .inflate(LayoutInflater.from(context)).also { it.data = this }.root
 }
 
 object UpdateChannel : BaseSettingsItem.Selector() {
-    override var value = Config.updateChannel.let { if (it < 0) 0 else it }
-        set(value) = setV(value, field, { field = it }) {
-            Config.updateChannel = it
+    override var value
+        get() = Config.updateChannel
+        set(value) {
+            Config.updateChannel = value
             Info.remote = Info.EMPTY_REMOTE
         }
 
@@ -154,18 +154,17 @@ object UpdateChannel : BaseSettingsItem.Selector() {
 
 object UpdateChannelUrl : BaseSettingsItem.Input() {
     override val title = R.string.settings_update_custom.asText()
-    override var value = Config.customChannelUrl
-        set(value) = setV(value, field, { field = it }) {
-            Config.customChannelUrl = it
-            Info.remote = Info.EMPTY_REMOTE
-        }
     override val description get() = value.asText()
+    override var value
+        get() = Config.customChannelUrl
+        set(value) {
+            Config.customChannelUrl = value
+            Info.remote = Info.EMPTY_REMOTE
+            notifyPropertyChanged(BR.description)
+        }
 
-    override val inputResult get() = result
-
-    @get:Bindable
-    var result = value
-        set(value) = set(value, field, { field = it }, BR.result)
+    override var inputResult: String = value
+        set(value) = set(value, field, { field = it }, BR.inputResult)
 
     override fun refresh() {
         isEnabled = UpdateChannel.value == Config.Value.CUSTOM_CHANNEL
@@ -178,9 +177,10 @@ object UpdateChannelUrl : BaseSettingsItem.Input() {
 object UpdateChecker : BaseSettingsItem.Toggle() {
     override val title = R.string.settings_check_update_title.asText()
     override val description = R.string.settings_check_update_summary.asText()
-    override var value = Config.checkUpdate
-        set(value) = setV(value, field, { field = it }) {
-            Config.checkUpdate = it
+    override var value
+        get() = Config.checkUpdate
+        set(value) {
+            Config.checkUpdate = value
             JobService.schedule(AppContext)
         }
 }
@@ -188,49 +188,12 @@ object UpdateChecker : BaseSettingsItem.Toggle() {
 object DoHToggle : BaseSettingsItem.Toggle() {
     override val title = R.string.settings_doh_title.asText()
     override val description = R.string.settings_doh_description.asText()
-    override var value = Config.doh
-        set(value) = setV(value, field, { field = it }) {
-            Config.doh = it
-        }
+    override var value by Config::doh
 }
 
-// check whether is module already installed beforehand?
 object SystemlessHosts : BaseSettingsItem.Blank() {
     override val title = R.string.settings_hosts_title.asText()
     override val description = R.string.settings_hosts_summary.asText()
-}
-
-object Tapjack : BaseSettingsItem.Toggle() {
-    override val title = R.string.settings_su_tapjack_title.asText()
-    override var description = R.string.settings_su_tapjack_summary.asText()
-    override var value = Config.suTapjack
-        set(value) = setV(value, field, { field = it }) { Config.suTapjack = it }
-}
-
-object Biometrics : BaseSettingsItem.Toggle() {
-    override val title = R.string.settings_su_biometric_title.asText()
-    override var value = Config.suBiometric
-        set(value) = setV(value, field, { field = it }) { Config.suBiometric = it }
-    override var description = R.string.settings_su_biometric_summary.asText()
-
-    override fun refresh() {
-        isEnabled = BiometricHelper.isSupported
-        if (!isEnabled) {
-            value = false
-            description = R.string.no_biometric.asText()
-        }
-    }
-}
-
-object Reauthenticate : BaseSettingsItem.Toggle() {
-    override val title = R.string.settings_su_reauth_title.asText()
-    override val description = R.string.settings_su_reauth_summary.asText()
-    override var value = Config.suReAuth
-        set(value) = setV(value, field, { field = it }) { Config.suReAuth = it }
-
-    override fun refresh() {
-        isEnabled = Build.VERSION.SDK_INT < Build.VERSION_CODES.O && Utils.showSuperUser()
-    }
 }
 
 // --- Magisk
@@ -244,10 +207,12 @@ object Zygisk : BaseSettingsItem.Toggle() {
     override val description get() =
         if (mismatch) R.string.reboot_apply_change.asText()
         else R.string.settings_zygisk_summary.asText()
-    override var value = Config.zygisk
-        set(value) = setV(value, field, { field = it }) {
-            Config.zygisk = it
-            Config.magiskHide = it
+    override var value
+        get() = Config.zygisk
+        set(value) {
+            Config.zygisk = value
+            Config.magiskHide = value
+            notifyPropertyChanged(BR.description)
         }
     val mismatch get() = value != Info.isZygiskEnabled
 }
@@ -256,16 +221,45 @@ object MagiskHide : BaseSettingsItem.Toggle() {
     override val title = R.string.magiskhide.asText()
     override val description = R.string.settings_magiskhide_summary.asText()
     override var value = Config.magiskHide
-        set(value) = setV(value, field, { field = it }) {
-            val cmd = if (it) "enable" else "disable"
+        set(value) {
+            field = value
+            val cmd = if (value) "enable" else "disable"
             Shell.su("magiskhide $cmd").submit { cb ->
-                if (cb.isSuccess) Config.magiskHide = it
-                else field = !it
+                if (cb.isSuccess) {
+                    Config.magiskHide = value
+                } else {
+                    field = !value
+                    notifyPropertyChanged(BR.checked)
+                }
             }
         }
+    override fun refresh() {
+        isEnabled = Zygisk.value
+    }
 }
 
 // --- Superuser
+
+object Tapjack : BaseSettingsItem.Toggle() {
+    override val title = R.string.settings_su_tapjack_title.asText()
+    override val description = R.string.settings_su_tapjack_summary.asText()
+    override var value by Config::suTapjack
+}
+
+object Biometrics : BaseSettingsItem.Toggle() {
+    override val title = R.string.settings_su_biometric_title.asText()
+    override var description = R.string.settings_su_biometric_summary.asText()
+    override var value by Config::suBiometric
+
+    override fun refresh() {
+        isEnabled = BiometricHelper.isSupported
+        if (!isEnabled) {
+            value = false
+            description = R.string.no_biometric.asText()
+            notifyPropertyChanged(BR.checked)
+        }
+    }
+}
 
 object Superuser : BaseSettingsItem.Section() {
     override val title = R.string.superuser.asText()
@@ -274,22 +268,14 @@ object Superuser : BaseSettingsItem.Section() {
 object AccessMode : BaseSettingsItem.Selector() {
     override val title = R.string.superuser_access.asText()
     override val entryRes = R.array.su_access
-
-    override var value = Config.rootMode
-        set(value) = setV(value, field, { field = it }) {
-            Config.rootMode = it
-        }
+    override var value by Config::rootMode
 }
 
 object MultiuserMode : BaseSettingsItem.Selector() {
     override val title = R.string.multiuser_mode.asText()
     override val entryRes = R.array.multiuser_mode
     override val descriptionRes = R.array.multiuser_summary
-
-    override var value = Config.suMultiuserMode
-        set(value) = setV(value, field, { field = it }) {
-            Config.suMultiuserMode = it
-        }
+    override var value by Config::suMultiuserMode
 
     override fun refresh() {
         isEnabled = Const.USER_ID == 0
@@ -300,21 +286,13 @@ object MountNamespaceMode : BaseSettingsItem.Selector() {
     override val title = R.string.mount_namespace_mode.asText()
     override val entryRes = R.array.namespace
     override val descriptionRes = R.array.namespace_summary
-
-    override var value = Config.suMntNamespaceMode
-        set(value) = setV(value, field, { field = it }) {
-            Config.suMntNamespaceMode = it
-        }
+    override var value by Config::suMntNamespaceMode
 }
 
 object AutomaticResponse : BaseSettingsItem.Selector() {
     override val title = R.string.auto_response.asText()
     override val entryRes = R.array.auto_response
-
-    override var value = Config.suAutoResponse
-        set(value) = setV(value, field, { field = it }) {
-            Config.suAutoResponse = it
-        }
+    override var value by Config::suAutoResponse
 }
 
 object RequestTimeout : BaseSettingsItem.Selector() {
@@ -322,21 +300,25 @@ object RequestTimeout : BaseSettingsItem.Selector() {
     override val entryRes = R.array.request_timeout
 
     private val entryValues = listOf(10, 15, 20, 30, 45, 60)
-    override var value = selected
-        set(value) = setV(value, field, { field = it }) {
-            Config.suDefaultTimeout = entryValues[it]
+    override var value = entryValues.indexOfFirst { it == Config.suDefaultTimeout }
+        set(value) {
+            field = value
+            Config.suDefaultTimeout = entryValues[value]
         }
-
-    private val selected: Int
-        get() = entryValues.indexOfFirst { it == Config.suDefaultTimeout }
 }
 
 object SUNotification : BaseSettingsItem.Selector() {
     override val title = R.string.superuser_notification.asText()
     override val entryRes = R.array.su_notification
+    override var value by Config::suNotification
+}
 
-    override var value = Config.suNotification
-        set(value) = setV(value, field, { field = it }) {
-            Config.suNotification = it
-        }
+object Reauthenticate : BaseSettingsItem.Toggle() {
+    override val title = R.string.settings_su_reauth_title.asText()
+    override val description = R.string.settings_su_reauth_summary.asText()
+    override var value by Config::suReAuth
+
+    override fun refresh() {
+        isEnabled = Build.VERSION.SDK_INT < Build.VERSION_CODES.O && Utils.showSuperUser()
+    }
 }